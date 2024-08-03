54362ecd88c189571715912ee6792652a9f123d3
deborasamara <deborasamara.ro@gmail.com>
2024-08-03 19:18:10 -0300

    atualizações

---
+++ bin/uptime.cpp
@@ -0,0 +1,14 @@
+/*
+CHAMADA UPTIME - Mostra a quanto tempo o computador está ligado
+*/
+
+#include <sys/sysinfo.h> // relacionado a declarações de funções de infos do sistema
+#include <iostream> // entrada e saída c++
+
+int main () // código 
+{
+    struct sysinfo info; // variavel do tipo struct sysinfo, onde sysinfo tem várias informações do sistema, como tempo de atividade, quantidade de memória livre, etc
+    sysinfo(&info); //a função sysinfo preenche info com informações do sistema atual. é colocado o endereço de memória de info no argumento
+    std::cout << "Uptime (tempo em segundos desde que foi iniciado ao tempo que foi chamada a função) = " << info.uptime << " segundos " << std::endl;
+    return 0; // funcionou
+}
--- shell.cpp
+++ shell.cpp
@@ -3,11 +3,16 @@
 #include <sys/wait.h> // gerenciamento de processos filho 
 #include <iostream> // funcionalidade de entrada e saída std::cout , std::cin
 #include <stdlib.h> // para recuperar o caminho do diretório atual
+#include <sstream> // manipulação de strings
+#include <vector> // vetores
+#include <string> // para manipular strings
+ 
+std::vector<std::string> vector_history; // vetor de comandos acionador
 
 // COMANDOS INTERNOS
 void process_pwd(){ // mostrar diretorio de trabalho
-    std::string dir_atual;
-    getcwd(dir_atual);
+    char dir_atual[1048];
+    getcwd(dir_atual, sizeof(dir_atual));
     std::cout <<"Diretório atual: " << dir_atual << std::endl;
 }
 
@@ -20,53 +25,67 @@ void process_cd(std::string directory){ // mudar diretório
 }
 
 void process_history(){ // histórico de comandos digitados pelo usuário
-
+    if(vector_history.empty()){// se o vetor estiver vazio
+        std::cout << "Nenhum comando executado ainda.";
+    }else{ // se o vetor tiver alguma coisa
+        std::cout << "histórico: " << std::endl;
+        for (size_t i = 0; i < vector_history.size(); ++i) {
+            std::cout << i + 1 << ": " << vector_history[i] << std::endl;
+        }
+    }
     
 }
 
 // PROCESSADOR DE COMANDOS
 void process_command(std::string command) { // recebe texto de comando
+    vector_history.push_back(command); // comando adicionado ao history
     // Se for comando interno...
+
+    // verificador de espaços
+    std::string principal_command;
+    std::string arg2;
+
+    size_t position_space = command.find(' ');
+    principal_command = command.substr(0, position_space);
+    arg2 = command.substr(pos + 1); // +1 para pular o espaço
+
+
     if (command == "exit"){ // se a string for "exit", finaliza. 
         exit(0); 
     }else if(command == "pwd"){
         process_pwd();
     }else if(command == "cd"){ // por verificação de algo digitadoo + string com o nome do diretorio para mudar
-        process_cd();
-        
+        process_cd(arg2);
+
     }else if(command == "history"){
+        process_history();
         
-    }else{ // comando externo
-        
-    }
-    
-
-    // Se for comando externo
-    // ∗ necessário verificar se é para ser executado em background
-    //  Se for caminho relativo, procurar o comando na lista de diretórios
-    //  Se for absoluto verifica se comando existe
-
-    std::string absolute_path = "/bin/" + command; // caminho absoluto para o comando
-    if (access(absolute_path.c_str(), F_OK) == 0) { // Arquivo existe no diretório, verificar se é executável
-        if (access(absolute_path.c_str(), X_OK) == 0) { // Arquivo é executável
-            pid_t pid = fork(); // cria processo e retorna id do processo filho
-            if (pid < 0){ // Erro - retornou -1. (Pid = -1)
-                std::cout << "Erro de execução!" << std::endl;
-                return;
-            } else if (pid == 0){ //processo filho. (Pid = 0)
-                char * argv[2] = {(char *)command.c_str(), nullptr}; // converte o código do comando em array de strings
-                execve(absolute_path.c_str(), argv, NULL); // executa o programa no caminho absoluto
-            } else { // Processo pai. (Pid > 0)
-                // Deve adicionar processo filho na lista (std::vector)
-                // de processos em execução para gerenciar background. ∗/
-                // Processo pai espera processo filho terminar. ∗/
-                // waitpid(pid, nullptr, 0);
+    }else{ // COMANDO EXTERNO
+        // Se for comando externo
+        // ∗ necessário verificar se é para ser executado em background
+        //  Se for absoluto verifica se comando existe
+        std::string absolute_path = "/bin/" + command; // caminho absoluto para o comando
+        if (access(absolute_path.c_str(), F_OK) == 0) { // Arquivo existe no diretório, verificar se é executável
+            if (access(absolute_path.c_str(), X_OK) == 0) { // Arquivo é executável
+                pid_t pid = fork(); // cria processo e retorna id do processo filho
+                if (pid < 0){ // Erro - retornou -1. (Pid = -1)
+                    std::cout << "Erro de execução!" << std::endl;
+                    return;
+                } else if (pid == 0){ //processo filho. (Pid = 0)
+                    char * argv[2] = {(char *)command.c_str(), nullptr}; // converte o código do comando em array de strings
+                    execve(absolute_path.c_str(), argv, NULL); // executa o programa no caminho absoluto
+                } else { // Processo pai. (Pid > 0)
+                    // Deve adicionar processo filho na lista (std::vector)
+                    // de processos em execução para gerenciar background. ∗/
+                    // Processo pai espera processo filho terminar. ∗/
+                    // waitpid(pid, nullptr, 0);
+                }
+            } else { // Arquivo não é executável
+                std::cout << "permission denied: " << command << std::endl;
             }
-        } else { // Arquivo não é executável
-            std::cout << "permission denied: " << command << std::endl;
+        } else { // Arquivo não existe
+            std::cout << "Command not found: " << command << std::endl;
         }
-    } else { // Arquivo não existe
-        std::cout << "Command not found: " << command << std::endl;
     }
 }
 //      Leitura do que o usuário quer
